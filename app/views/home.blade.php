e3d40695d4f283d4b09be9727c6b9b5f31745ef3
dearlordlt <sarunas79@gmail.com>
2014-11-25 16:03:31 +0200

    Home layout mockup

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,9 +1,99 @@
 @extends('layout.main')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h4>Rules</h4>
+                    <ul class="list-unstyled">
+                        <li><a>Vigor</a></li>
+                        <li><a>Attributes</a></li>
+                        <li><a>Fatigue</a></li>
+                        <li><a>Breaking shields</a></li>
+                        <li><a>Decapacitating foes</a></li>
+                    </ul>
+                    <hr>
+                    <a class="btn btn-primary btn-md" href="#" role="button"><span class="glyphicon glyphicon-plus-sign"></span> </a>
+                    <a class="btn btn-success btn-md" href="#" role="button"><span class="glyphicon glyphicon-list"></span> </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h4>Articles</h4>
+                    <ul class="list-unstyled">
+                        <li><a>Basic combat</a></li>
+                        <li><a>Advanced combat</a></li>
+                        <li><a>Creating new character</a></li>
+                        <li><a>How to be a master</a></li>
+                        <li><a>How to osom</a></li>
+                    </ul>
+                    <hr>
+                    <a class="btn btn-primary btn-md" href="#" role="button"><span class="glyphicon glyphicon-plus-sign"></span> </a>
+                    <a class="btn btn-success btn-md" href="#" role="button"><span class="glyphicon glyphicon-list"></span> </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h4>Weapons</h4>
+                    <ul class="list-unstyled">
+                        <li><a>Broadsword</a></li>
+                        <li><a>Katana</a></li>
+                        <li><a>Tower shield</a></li>
+                        <li><a>Lightsaber (red)</a></li>
+                        <li><a>Vytauto kalavijas</a></li>
+                    </ul>
+                    <hr>
+                    <a class="btn btn-primary btn-md" href="#" role="button"><span class="glyphicon glyphicon-plus-sign"></span> </a>
+                    <a class="btn btn-success btn-md" href="#" role="button"><span class="glyphicon glyphicon-list"></span> </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h4>Rules</h4>
+                    <ul class="list-unstyled">
+                        <li><a>Vigor</a></li>
+                        <li><a>Attributes</a></li>
+                        <li><a>Fatigue</a></li>
+                        <li><a>Breaking shields</a></li>
+                        <li><a>Decapacitating foes</a></li>
+                    </ul>
+                    <hr>
+                    <a class="btn btn-primary btn-md" href="#" role="button"><span class="glyphicon glyphicon-plus-sign"></span> </a>
+                    <a class="btn btn-success btn-md" href="#" role="button"><span class="glyphicon glyphicon-list"></span> </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h4>Articles</h4>
+                    <ul class="list-unstyled">
+                        <li><a>Basic combat</a></li>
+                        <li><a>Advanced combat</a></li>
+                        <li><a>Creating new character</a></li>
+                        <li><a>How to be a master</a></li>
+                        <li><a>How to osom</a></li>
+                    </ul>
+                    <hr>
+                    <a class="btn btn-primary btn-md" href="#" role="button"><span class="glyphicon glyphicon-plus-sign"></span> </a>
+                    <a class="btn btn-success btn-md" href="#" role="button"><span class="glyphicon glyphicon-list"></span> </a>
+                </div>
+            </div>
+        </div>
+    </div>
 	@if(Auth::check())
 		<div class="well">Hello, <b>{{ Auth::user()->username }}</b></div>
 	@else
-		<div class="well">You are not signed in.</div>
+        <div class="well">You are not signed in.</div>
 	@endif
 @stop
--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap -->
     <link href="{{ URL::route('home') }}/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ URL::route('home') }}/css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
