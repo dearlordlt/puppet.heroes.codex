7f75e4f9f237e6b8b6ef637b5a5ad1cae4fbaa50
dearlordlt <sarunas79@gmail.com>
2014-11-26 12:43:19 +0200

    Books view added

--- app/views/books/books.blade.php
+++ app/views/books/books.blade.php
@@ -1,7 +1,25 @@
 @extends('layout.main')
     <div class="container-fluid">
         <h4>Books</h4>
-        <pre><?php printf ($books); ?></pre>
+        <ul class="list-group">
+            @foreach ($books as $book)
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">{{ $book->name }}</h4>
+                    <p class="list-group-item-text">
+                        {{ $book->description }}
+                    </p>
+                </a>
+            @endforeach
+        </ul>
+
+        @if(Auth::check())
+            <div class="well">
+                <h4 class="list-group-item-heading"></h4>
+                <p class="list-group-item-text">
+                    <a class="btn btn-primary btn-md" href="#" role="button"><span class="glyphicon glyphicon-plus-sign"></span> </a>
+                </p>
+            </div>
+        @endif
     </div>
 @section('content')
 
